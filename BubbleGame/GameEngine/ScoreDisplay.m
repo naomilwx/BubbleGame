7b10df1128bf109e353bcceb80d2e08c40761df1
Naomi Leow <naomileowwx@gmail.com>
2014-03-01 16:07:34 +0800

    changed score colour

--- BubbleGame/GameEngine/ScoreDisplay.m
+++ BubbleGame/GameEngine/ScoreDisplay.m
@@ -10,7 +10,7 @@
 #import "GameCommon.h"
 
 #define SCORE_FONT @"Helvetica-Bold"
-#define SCORE_FONT_SIZE 24
+#define SCORE_FONT_SIZE 30
 #define SCORE_TEXT @"Score: %d"
 
 @implementation ScoreDisplay{
@@ -22,7 +22,7 @@
         _gameView = view;
         [self initialiseScoreDisplayWithFrame:frame];
         displayedScore = 0;
-        [self setScoreDisplayWithScore:displayedScore];
+        [self setScoreDisplayToScore:displayedScore];
     }
     return self;
 }
@@ -34,7 +34,7 @@
 - (void)initialiseScoreDisplayWithFrame:(CGRect)frame{
     _scoreDisplay = [[UILabel alloc] initWithFrame:frame];
     _scoreDisplay.font = [UIFont fontWithName:SCORE_FONT size:SCORE_FONT_SIZE];
-    _scoreDisplay.adjustsFontSizeToFitWidth = YES;
+    _scoreDisplay.textColor = [UIColor whiteColor];
     [self.gameView addSubview:_scoreDisplay];
     NSNotificationCenter *note = [NSNotificationCenter defaultCenter];
     [note addObserver:self selector:@selector(receiveScoreUpdate:) name:SCORE_NOTIFICATION object:nil];
@@ -42,27 +42,12 @@
 
 - (void)receiveScoreUpdate:(NSNotification *)notification{
     NSInteger newScore = [[[notification userInfo] objectForKey:SCORE_NOTIFICATION] integerValue];
-    [self updateDisplayedScoreTo:newScore];
+    [self setScoreDisplayToScore:newScore];
 }
 
-- (void)setScoreDisplayWithScore:(NSInteger)score{
+- (void)setScoreDisplayToScore:(NSInteger)score{
+    displayedScore = score;
     [self.scoreDisplay setText:[NSString stringWithFormat:SCORE_TEXT, displayedScore]];
 }
 
-- (void)updateDisplayedScoreTo:(NSInteger)score{
-    NSInteger (^scoreCount)(NSInteger);
-    if(score < displayedScore){
-        scoreCount = ^NSInteger(NSInteger oldScore){
-            return displayedScore - 1;
-        };
-    }else{
-        scoreCount = ^NSInteger(NSInteger oldScore){
-            return displayedScore + 1;
-        };
-    }
-    while(displayedScore != score){
-        displayedScore = scoreCount(displayedScore);
-        [self setScoreDisplayWithScore:displayedScore];
-    }
-}
 @end
